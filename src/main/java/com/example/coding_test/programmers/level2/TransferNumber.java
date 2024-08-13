e4aa5ef4c2a1740e26c4cd7d01662d1a1cdb6c0c
Wonjuyeon <dnjswndus95@naver.com>
2024-08-13 12:34:39 +0900

    프로그래머스 level2 숫자 변환하기

---
+++ src/main/java/com/example/coding_test/programmers/level2/TransferNumber.java
@@ -0,0 +1,33 @@
+package com.example.coding_test.programmers.level2;
+
+/**
+ * 주소 : https://school.programmers.co.kr/learn/courses/30/lessons/154538
+ * 문제 : 숫자 변환하기
+ */
+public class TransferNumber {
+
+    public static void main(String[] args) {
+        solution(10, 40, 5);
+    }
+
+    public static int solution(int x, int y, int n) {
+        int[] dp = new int[y + 1];
+
+        for(int i = x; i <= y; ++i) {
+            if(i != x && dp[i] == 0) {
+                dp[i] = -1;
+                continue;
+            }
+
+            if(i * 2 <= y)
+                dp[i * 2] = dp[i * 2] == 0 ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
+            if(i * 3 <= y)
+                dp[i * 3] = dp[i * 3] == 0 ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
+            if(i + n <= y)
+                dp[i + n] = dp[i + n] == 0 ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
+        }
+
+
+        return dp[y];
+    }
+}
