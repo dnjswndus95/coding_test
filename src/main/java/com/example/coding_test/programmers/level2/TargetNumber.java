eea9427311659329f0767947648e731dcaa76810
Wonjuyeon <dnjswndus95@naver.com>
2024-10-03 21:31:33 +0900

    level2 타겟 넘버 dfs 다시 풀어보기

--- src/main/java/com/example/coding_test/programmers/level2/TargetNumber.java
+++ src/main/java/com/example/coding_test/programmers/level2/TargetNumber.java
@@ -10,27 +10,29 @@ public class TargetNumber {
     static int answer = 0;
     public static void main(String[] args) {
         int[] numbers = {1, 1, 1};
-        int target = 2;
+        int target = 1;
         solution(numbers, target);
     }
 
     public static int solution(int[] numbers, int target) {
+        // depth를 전달하니까 for문은 필요없음.
         dfs(numbers, 0, target, 0);
         return answer;
     }
 
-    public static void dfs(int[] numbers, int depth, int target, int currentValue) {
+    public static void dfs(int[] numbers, int depth, int target, int value) {
         if(depth == numbers.length) {
-            if(target == currentValue) {
+            if (target == value) {
                 ++answer;
             }
             return;
         }
 
-        int plusValue = currentValue + numbers[depth];
-        int minusValue = currentValue - numbers[depth];
+        int plusValue = value + numbers[depth];
+        int minusValue = value - numbers[depth];
 
         dfs(numbers, depth + 1, target, plusValue);
         dfs(numbers, depth + 1, target, minusValue);
     }
+
 }
