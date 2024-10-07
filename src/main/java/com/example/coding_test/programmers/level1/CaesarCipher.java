1ca7d1829cfdfca6dfe76fc50a10ee2ef2314ed9
Wonjuyeon <dnjswndus95@naver.com>
2024-10-07 17:31:46 +0900

    level1 시저 암호 풀이 완

---
+++ src/main/java/com/example/coding_test/programmers/level1/CaesarCipher.java
@@ -0,0 +1,22 @@
+package com.example.coding_test.programmers.level1;
+
+public class CaesarCipher {
+
+    public String solution(String s, int n) {
+        String answer = "";
+
+        for(int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+
+            if(Character.isUpperCase(ch)) {
+                ch = (char)((ch - 'A' + n) % 26 + 'A');
+            } else if(Character.isLowerCase(ch)) {
+                ch = (char)((ch - 'a' + n) % 26 + 'a');
+            }
+
+            answer += ch;
+        }
+
+        return answer;
+    }
+}
