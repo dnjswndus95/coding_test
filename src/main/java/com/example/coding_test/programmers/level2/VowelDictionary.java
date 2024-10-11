cc4ba687ca17e3edcd6fc1e12ab371a30a24e53e
Wonjuyeon <dnjswndus95@naver.com>
2024-10-11 15:55:54 +0900

    level2 모음 사전 풀이 수학공식으로 풀이

--- src/main/java/com/example/coding_test/programmers/level2/VowelDictionary.java
+++ src/main/java/com/example/coding_test/programmers/level2/VowelDictionary.java
@@ -1,38 +1,23 @@
 package com.example.coding_test.programmers.level2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class VowelDictionary {
 
     public static void main(String[] args) {
         solution("AAAE");
     }
 
-    static String[] strArray = {"A", "E", "I", "O", "U"};
-    static List<String> list = new ArrayList<>();
     public static int solution(String word) {
         int answer = 0;
 
-        dfs("", 0);
-        int size = list.size();
+        String indexStr = "AEIOU";
+        int per = 3905;
 
-        for(int i = 0; i < size; ++i) {
-            if(word.equals(list.get(i)))
-                return i;
+        for (String s : word.split("")) {
+            per /= 5;
+            answer += indexStr.indexOf(s) * per + 1;
         }
 
         return answer;
-    }
-
-    public static void dfs(String str, int length) {
-        list.add(str);
-
-        if(length == 5)
-            return;
-        for(int i = 0; i < strArray.length; ++i) {
-            dfs(str + strArray[i], length + 1);
-        }
     }
 
 }
